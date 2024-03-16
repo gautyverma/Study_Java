80caaa1a4009293b68c613f1b28c51bae4f8a6ff
Gauty <32440155+gautyverma@users.noreply.github.com>
2024-03-16 23:51:38 +0530

    Committing random old code

---
+++ Java_8_Features/src/main/java/a1_Lambda/a1_basic/Main.java
@@ -0,0 +1,22 @@
+package a1_Lambda.a1_basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<>();
+    list.add("Aakash");
+    list.add("Gautam");
+    list.add("Sarthak");
+    list.add("Vinay");
+    list.add("Harsh");
+    // Print using for loop
+    for (String item : list) {
+      System.out.println(item);
+    }
+    System.out.println("---- Print using for loop using lambda operation ----");
+    // Print using for loop using lambda operation
+    list.forEach(item -> System.out.println(item));
+  }
+}

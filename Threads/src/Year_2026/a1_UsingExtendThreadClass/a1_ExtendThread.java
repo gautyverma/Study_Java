a0958f49f4bb433210d17c9091b610fe9cec00f3
GAUTY <gautyverma@gmail.com>
2026-04-16 21:22:41 +0530

    [Multi-threading] - a1_ExtendThread

---
+++ Threads/src/Year_2026/a1_UsingExtendThreadClass/a1_ExtendThread.java
@@ -0,0 +1,41 @@
+package Year_2026.a1_UsingExtendThreadClass;
+
+import Year_2026.ThreadColor;
+
+/*
+* Output possibility:
+
+Running inside main thread class
+Running inside main thread class
+Running inside extended thread class
+Running inside extended thread class
+Running inside main thread class
+Running inside main thread class
+Running inside extended thread class
+Running inside extended thread class
+Running inside extended thread class
+
+* */
+public class a1_ExtendThread {
+  public static void main(String[] args) {
+    MyThread thread = new MyThread();
+    thread.start();
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println(ThreadColor.ANSI_PURPLE + "Running inside main thread class");
+    }
+    /*
+    // No new thread will be created, run() will be executed just like normal method call.
+          thread.run();
+    */
+  }
+
+  static class MyThread extends Thread {
+    @Override
+    public void run() {
+      for (int i = 0; i < 5; i++) {
+        System.out.println(ThreadColor.ANSI_BLUE + "Running inside extended thread class");
+      }
+    }
+  }
+}
